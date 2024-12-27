e7feca9fb08e9e1d8da4e56a8d885e18717f63a3
Brijesh-jethava <brijeshjethva900@gmail.com>
2024-12-27 10:37:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
 import logo from './logo.svg';
 import './App.css';
+import 'react-notifications/lib/notifications.css';
+import {NotificationContainer, NotificationManager} from 'react-notifications'
+import { useState } from 'react';
+import { data } from './data';
 
 function App() {
+  let[todolist , setTodolist] = useState([])
+  let[active,setActive] = useState(0)
+  let[disc, setDisc] = useState(data[0])
+
+  let saveToDolist = (event)=>
+    {
+      let toName = event.target.txtbox.value;
+      // alert(toName);
+      if(!todolist.includes(toName))
+        {
+           let finalTodolist = [...todolist , toName]
+           setTodolist(finalTodolist)
+        }
+        else
+        {
+          NotificationManager.error("ToDolist already exists");
+        }
+      event.preventDefault();  //To avoid Refreshing of page at save time
+
+    }
+
+    let list = todolist.map((val,i)=>{
+    return(
+      <ToDoListItems value ={val} key={i} indexNumber = {i} todolist={todolist} setTodolist={setTodolist}/>
+    ) 
+   })
+
+
+   
+  // let btn = data.map((val,i)=>{
+  //   function toSet ()
+  //   {
+  //     setActive(i);
+  //     setDisc(data[i]);
+  //   }
+  //    return(
+  //     <button  onClick={toSet} className = {i == active ?'activeButtons':''} > {val.title} </button>
+     
+  //    )
+  // })
+
+  
+   
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    
+
+      {/* Active Tabs */}
+      {/* <div className="activeTabs">
+        <div className="btns">
+         {btn}
+        </div>
+
+        <div className="content" >
+        <p>{disc.discription}</p>
+        </div>
+        
+      </div> */}
+
+    {/* ToDoList */}
+    <NotificationContainer/>
+      <h1>ToDo List</h1>
+      <form onSubmit = {saveToDolist}>
+        <input type="text"  name='txtbox' placeholder='Add Task'/>
+        <button>Save</button>
+      </form>
+
+     <div className="outerDiv">
+      <ul>
+        {list}
+      </ul>
+     </div>
     </div>
   );
 }
 
 export default App;
+
+function ToDoListItems({value,indexNumber,todolist,setTodolist})
+{
+  let[status,setStatus] = useState(false)
+  let deletRow = ()=>
+    {
+      let finaldata = todolist.filter((val,i)=>i!=indexNumber)
+      setTodolist(finaldata)
+    }
+  
+  let checkStatus = ()=>
+    {
+      setStatus(!status)
+    }
+  return(
+    <li className={(status) ? 'completeTodo':''} onClick={checkStatus}>{indexNumber+1}. {value}<span onClick={deletRow}>&times;</span></li>
+  )
+}
+ 
---
+++ src/data.jsx
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        'title' : 'Vision',
+        'discription': "To be a global leader in the IT industry, recognized for our commitment to excellence, innovation, and sustainability. We envision a future where technology seamlessly integrates into every aspect of life, creating smarter, more connected communities and businesses."
+    },
+    {
+        'title' : 'Mission',
+        'discription':"Our mission is to empower businesses and individuals through cutting-edge technology solutions, fostering innovation, efficiency, and growth. We aim to provide reliable, secure, and scalable IT services that drive success and transform ideas into reality."
+    },
+    {
+        'title' : 'Values',
+        'discription':"We are committed to innovation, integrity, and excellence, placing our clients at the center of everything we do. Collaboration and respect for diversity drive our inclusive and supportive work environment, while sustainability and ethical practices guide our business operations. We embrace agility and continuous learning to stay ahead in the dynamic IT landscape, ensuring we deliver forward-thinking solutions that foster growth and success for our clients and communities."
+    }
+]
